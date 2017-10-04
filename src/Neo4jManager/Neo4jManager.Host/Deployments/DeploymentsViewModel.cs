4cb23d36fb668ea9f1a464bb74f7a7ddd70aad7d
Neil Dobson <ndobson@barnardos.org.au>
2017-10-04 21:19:38 +1100

    Add Home module and dashboard view

---
+++ src/Neo4jManager/Neo4jManager.Host/Deployments/DeploymentsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Neo4jManager.Host.Deployments
+{
+    public class DeploymentsViewModel
+    {
+        public IEnumerable<Deployment> Deployments { get; set; }
+    }
+}
---
+++ src/Neo4jManager/Neo4jManager.Host/HomeModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using Nancy;
+using Neo4jManager.Host.Deployments;
+
+namespace Neo4jManager.Host
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule(INeo4jDeploymentsPool pool, IMapper mapper) : base("")
+        {
+            // Get all deployments
+            Get("/", _ =>
+            {
+                var viewModel = new DeploymentsViewModel
+                {
+                    Deployments = mapper.Map<IEnumerable<Deployment>>(pool.Deployments)
+                };
+
+                return Negotiate
+                    .WithModel(viewModel)
+                    .WithView("Home");
+            });
+        }
+    }
+}
